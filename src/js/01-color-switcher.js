847b6249d8158c203699195ef4f57b043bb1e02f
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-09-13 18:52:50 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const selectors = {
+    start: document.querySelector("[data-start]"),
+    stop: document.querySelector("[data-stop]"),
+body: document.querySelector("body")
+}
 
+let timerId = null
+let isActive = false
+
+selectors.start.addEventListener('click', onStart)
+selectors.stop.addEventListener('click', onStop)
+
+function onStart() {
+    if (isActive) {
+        return
+    }
+    isActive = true
+    selectors.start.disabled = true;
+    selectors.stop.disabled = false;
+
+    timerId = setInterval(() =>
+        {selectors.body.style.backgroundColor = getRandomHexColor()}
+, 1000)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onStop() {
+    clearInterval(timerId)
+    isActive = false
+    selectors.start.disabled = false;
+    selectors.stop.disabled = true;
+}
